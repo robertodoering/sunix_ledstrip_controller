c5d68200a9f8e05a08fb8a9ee0469148b9932fb0
Roberto Doering <rbydoering@gmail.com>
2018-09-23 01:39:42 +0200

    Initial commit

---
+++ example/sunix_ledstrip_controller_example.dart
@@ -0,0 +1,6 @@
+import 'package:sunix_ledstrip_controller/sunix_ledstrip_controller.dart';
+
+main() {
+  var awesome = new Awesome();
+  print('awesome: ${awesome.isAwesome}');
+}
---
+++ lib/sunix_ledstrip_controller.dart
@@ -0,0 +1,8 @@
+/// Support for doing something awesome.
+///
+/// More dartdocs go here.
+library sunix_ledstrip_controller;
+
+export 'src/sunix_ledstrip_controller_base.dart';
+
+// TODO: Export any libraries intended for clients of this package.
